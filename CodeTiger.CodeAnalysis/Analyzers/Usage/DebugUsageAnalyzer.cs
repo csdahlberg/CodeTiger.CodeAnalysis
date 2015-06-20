3723a3328e3f1d502ae4ec484bb42a0b560a2405
Chris Dahlberg <csdahlberg@gmail.com>
2015-06-20 09:14:29 -0500

    Added usage-related diagnostics for including an inner exception when throwing within a catch block, including a justification when using the SuppressMessage attribute, and including a message when calling the Debug.Assert method.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Usage/DebugUsageAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Usage
+{
+    /// <summary>
+    /// Analyzes usage of the <see cref="Debug"/> class.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DebugUsageAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor CallsToDebugAssertShouldIncludeAMessageDescriptor
+            = new DiagnosticDescriptor("CT2202", "Calls to the Debug.Assert method should include a message.",
+                "Calls to the Debug.Assert method should include a message.", "CodeTiger.Usage",
+                DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Returns a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(CallsToDebugAssertShouldIncludeAMessageDescriptor);
+            }
+        }
+
+        /// <summary>
+		/// Registers actions in an analysis context.
+		/// </summary>
+		/// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.RegisterSemanticModelAction(AnalyzeDebugUsage);
+        }
+
+        private void AnalyzeDebugUsage(SemanticModelAnalysisContext context)
+        {
+            var root = context.SemanticModel.SyntaxTree.GetRoot(context.CancellationToken);
+            var debugType = context.SemanticModel.Compilation
+                .GetTypeByMetadataName("System.Diagnostics.Debug");
+
+            var invocations = root.DescendantNodes().OfType<InvocationExpressionSyntax>();
+
+            foreach (var invocation in invocations
+                .Where(x => x.Expression.Kind() == SyntaxKind.SimpleMemberAccessExpression))
+            {
+                var memberAccessExpression = (MemberAccessExpressionSyntax)invocation.Expression;
+
+                var targetType = context.SemanticModel.GetTypeInfo(memberAccessExpression.Expression);
+                if (targetType.Type == debugType && memberAccessExpression?.Name?.Identifier.Text == "Assert")
+                {
+                    // NOTE: This assumes that the message is always the second argument.
+                    if (invocation.ArgumentList.Arguments.Count < 2)
+                    {
+                        context.ReportDiagnostic(Diagnostic.Create(
+                            CallsToDebugAssertShouldIncludeAMessageDescriptor, invocation.GetLocation()));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CodeTiger.CodeAnalysis/Analyzers/Usage/SuppressMessageAttributeUsageAnalyzer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Usage
+{
+    /// <summary>
+    /// Analyzes usage of the <see cref="SuppressMessageAttribute"/> class.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SuppressMessageAttributeUsageAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor SuppressMessageAttributeShouldIncludeJustificationDescriptor
+            = new DiagnosticDescriptor("CT2201",
+                "Use of the SuppressMessage attribute should include justification.",
+                "Use of the SuppressMessage attribute should include justification.",
+                "CodeTiger.Usage", DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Returns a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(SuppressMessageAttributeShouldIncludeJustificationDescriptor);
+            }
+        }
+
+        /// <summary>
+		/// Registers actions in an analysis context.
+		/// </summary>
+		/// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.RegisterSemanticModelAction(AnalyzeSuppressMessageAttributeUsage);
+        }
+
+        private void AnalyzeSuppressMessageAttributeUsage(SemanticModelAnalysisContext context)
+        {
+            var root = context.SemanticModel.SyntaxTree.GetRoot(context.CancellationToken);
+            var suppressMessageAttributeType = context.SemanticModel.Compilation
+                .GetTypeByMetadataName("System.Diagnostics.CodeAnalysis.SuppressMessageAttribute");
+
+            var attributeUsages = root.DescendantNodes().OfType<AttributeSyntax>();
+
+            foreach (var attributeUsage in attributeUsages)
+            {
+                var attributeType = context.SemanticModel.GetTypeInfo(attributeUsage);
+                if (attributeType.Type == suppressMessageAttributeType
+                    && !AttributeIncludesJustificationArgument(attributeUsage))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(
+                        SuppressMessageAttributeShouldIncludeJustificationDescriptor,
+                        attributeUsage.GetLocation()));
+                }
+            }
+        }
+
+        private static bool AttributeIncludesJustificationArgument(AttributeSyntax attributeUsage)
+        {
+            return attributeUsage.ArgumentList.Arguments
+                .Any(arg => arg.NameEquals?.Name?.Identifier.Text == "Justification");
+        }
+    }
+}
