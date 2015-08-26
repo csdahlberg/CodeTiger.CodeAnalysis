5f3ce641d000272e7050facc5f4e82dc61aec3ba
csdahlberg <csdahlberg@gmail.com>
2015-08-26 18:54:56 -0500

    Added CT3100 to identify empty statements.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Readability/GeneralReadabilityAnalyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Readability
+{
+    /// <summary>
+    /// Analyzes general readability issues.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class GeneralReadabilityAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor EmptyStatementsShouldNotBeUsedDescriptor
+            = new DiagnosticDescriptor("CT3100", "Empty statements should not be used.",
+                "Empty statements should not be used.", "CodeTiger.Readability", DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(EmptyStatementsShouldNotBeUsedDescriptor);
+            }
+        }
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.RegisterSyntaxNodeAction(AnalyzeEmptyStatements, SyntaxKind.EmptyStatement);
+        }
+
+        private void AnalyzeEmptyStatements(SyntaxNodeAnalysisContext context)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(EmptyStatementsShouldNotBeUsedDescriptor,
+                context.Node.GetLocation()));
+        }
+    }
+}
