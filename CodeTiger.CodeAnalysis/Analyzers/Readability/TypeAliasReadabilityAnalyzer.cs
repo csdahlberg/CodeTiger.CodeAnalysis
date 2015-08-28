e1aed663e2027850aaf0759de3662b67b9c8c345
csdahlberg <csdahlberg@gmail.com>
2015-08-27 20:44:13 -0500

    Added CT3101 to identify use of .NET type names instead of built-in C# type aliases.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Readability/TypeAliasReadabilityAnalyzer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Readability
+{
+    /// <summary>
+    /// Analyzes declarations for the use of built-in type aliases.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TypeAliasReadabilityAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor BuiltInTypeAliasesShouldBeUsedDescriptor
+            = new DiagnosticDescriptor("CT3101", "Built-in type aliases should be used.",
+                "Built-in type alises should be used.", "CodeTiger.Readability", DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(BuiltInTypeAliasesShouldBeUsedDescriptor);
+            }
+        }
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.IdentifierName);
+        }
+
+        private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var symbol = context.SemanticModel.GetSymbolInfo(context.Node, context.CancellationToken).Symbol;
+            if (symbol?.Kind != SymbolKind.NamedType)
+            {
+                return;
+            }
+
+            var namedTypeSymbol = (INamedTypeSymbol)symbol;
+
+            switch (namedTypeSymbol.SpecialType)
+            {
+                case SpecialType.System_Boolean:
+                case SpecialType.System_Char:
+                case SpecialType.System_SByte:
+                case SpecialType.System_Byte:
+                case SpecialType.System_Int16:
+                case SpecialType.System_UInt16:
+                case SpecialType.System_Int32:
+                case SpecialType.System_UInt32:
+                case SpecialType.System_Int64:
+                case SpecialType.System_UInt64:
+                case SpecialType.System_Decimal:
+                case SpecialType.System_Single:
+                case SpecialType.System_Double:
+                case SpecialType.System_String:
+                    context.ReportDiagnostic(Diagnostic.Create(BuiltInTypeAliasesShouldBeUsedDescriptor,
+                        context.Node.GetLocation()));
+                    break;
+            }
+        }
+    }
+}
