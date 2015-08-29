da195691733452eb1359c613755ab728b1ef175c
csdahlberg <csdahlberg@gmail.com>
2015-08-29 10:05:35 -0500

    Added CT3104 to identify member declarations without a declared accessibility.

--- CodeTiger.CodeAnalysis/Analyzers/Readability/AccessibilityReadabilityAnalyzer.cs
+++ CodeTiger.CodeAnalysis/Analyzers/Readability/AccessibilityReadabilityAnalyzer.cs
@@ -17,6 +17,10 @@ namespace CodeTiger.CodeAnalysis.Analyzers.Readability
             = new DiagnosticDescriptor("CT3103", "Types should have an access modifier specified.",
                 "Types should have an access modifier specified.", "CodeTiger.Readability",
                 DiagnosticSeverity.Warning, true);
+        internal static readonly DiagnosticDescriptor MembersShouldHaveAccessModifierSpecifiedDescriptor
+            = new DiagnosticDescriptor("CT3104", "Members should have an access modifier specified.",
+                "Members should have an access modifier specified.", "CodeTiger.Readability",
+                DiagnosticSeverity.Warning, true);
 
         /// <summary>
         /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
@@ -25,7 +29,8 @@ namespace CodeTiger.CodeAnalysis.Analyzers.Readability
         {
             get
             {
-                return ImmutableArray.Create(TypesShouldHaveAccessModifierSpecifiedDescriptor);
+                return ImmutableArray.Create(TypesShouldHaveAccessModifierSpecifiedDescriptor,
+                    MembersShouldHaveAccessModifierSpecifiedDescriptor);
             }
         }
 
@@ -40,33 +45,118 @@ namespace CodeTiger.CodeAnalysis.Analyzers.Readability
 
             context.RegisterSyntaxNodeAction(AnalyzeTypeDeclaration, SyntaxKind.ClassDeclaration,
                 SyntaxKind.StructDeclaration, SyntaxKind.EnumDeclaration, SyntaxKind.InterfaceDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeMemberDeclaration, SyntaxKind.ConstructorDeclaration,
+                SyntaxKind.ConversionOperatorDeclaration, SyntaxKind.DelegateDeclaration,
+                SyntaxKind.EventDeclaration, SyntaxKind.EventFieldDeclaration, SyntaxKind.FieldDeclaration,
+                SyntaxKind.IndexerDeclaration, SyntaxKind.MethodDeclaration, SyntaxKind.OperatorDeclaration,
+                SyntaxKind.PropertyDeclaration);
         }
 
         private void AnalyzeTypeDeclaration(SyntaxNodeAnalysisContext context)
         {
-            var baseTypeDeclaration = (BaseTypeDeclarationSyntax)context.Node;
+            var modifiers = GetModifiers(context.Node);
 
-            SyntaxTokenList modifiers;
-
-            switch (baseTypeDeclaration.Kind())
-            {
-                case SyntaxKind.ClassDeclaration:
-                case SyntaxKind.StructDeclaration:
-                case SyntaxKind.InterfaceDeclaration:
-                    modifiers = ((TypeDeclarationSyntax)baseTypeDeclaration).Modifiers;
-                    break;
-                case SyntaxKind.EnumDeclaration:
-                    modifiers = ((EnumDeclarationSyntax)baseTypeDeclaration).Modifiers;
-                    break;
-                default:
-                    modifiers = new SyntaxTokenList();
-                    break;
-            }
+            var identifierLocation = GetIdentifierLocation(context.Node);
 
             if (!modifiers.Any(x => SyntaxFacts.IsAccessibilityModifier(x.Kind())))
             {
                 context.ReportDiagnostic(Diagnostic.Create(TypesShouldHaveAccessModifierSpecifiedDescriptor,
-                    baseTypeDeclaration.Identifier.GetLocation()));
+                    identifierLocation));
+            }
+        }
+
+        private void AnalyzeMemberDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var modifiers = GetModifiers(context.Node);
+
+            if (!modifiers.Any(x => SyntaxFacts.IsAccessibilityModifier(x.Kind()))
+                && !IsExplicitInterfaceImplementation(context.Node))
+            {
+                var identifierLocation = GetIdentifierLocation(context.Node);
+
+                context.ReportDiagnostic(Diagnostic.Create(MembersShouldHaveAccessModifierSpecifiedDescriptor,
+                    identifierLocation));
+            }
+        }
+
+        private static SyntaxTokenList GetModifiers(SyntaxNode node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.ClassDeclaration:
+                case SyntaxKind.StructDeclaration:
+                case SyntaxKind.InterfaceDeclaration:
+                    return ((TypeDeclarationSyntax)node).Modifiers;
+                case SyntaxKind.EnumDeclaration:
+                    return ((EnumDeclarationSyntax)node).Modifiers;
+                case SyntaxKind.ConstructorDeclaration:
+                case SyntaxKind.ConversionOperatorDeclaration:
+                case SyntaxKind.MethodDeclaration:
+                case SyntaxKind.OperatorDeclaration:
+                    return ((BaseMethodDeclarationSyntax)node).Modifiers;
+                case SyntaxKind.DelegateDeclaration:
+                    return ((DelegateDeclarationSyntax)node).Modifiers;
+                case SyntaxKind.EventDeclaration:
+                case SyntaxKind.IndexerDeclaration:
+                case SyntaxKind.PropertyDeclaration:
+                    return ((BasePropertyDeclarationSyntax)node).Modifiers;
+                case SyntaxKind.EventFieldDeclaration:
+                case SyntaxKind.FieldDeclaration:
+                    return ((BaseFieldDeclarationSyntax)node).Modifiers;
+                default:
+                    return new SyntaxTokenList();
+            }
+        }
+
+        private static Location GetIdentifierLocation(SyntaxNode node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.ClassDeclaration:
+                case SyntaxKind.StructDeclaration:
+                case SyntaxKind.InterfaceDeclaration:
+                case SyntaxKind.EnumDeclaration:
+                    return ((BaseTypeDeclarationSyntax)node).Identifier.GetLocation();
+                case SyntaxKind.ConstructorDeclaration:
+                    return ((ConstructorDeclarationSyntax)node).Identifier.GetLocation();
+                case SyntaxKind.ConversionOperatorDeclaration:
+                    return ((ConversionOperatorDeclarationSyntax)node).Type.GetLocation();
+                case SyntaxKind.MethodDeclaration:
+                    return ((MethodDeclarationSyntax)node).Identifier.GetLocation();
+                case SyntaxKind.OperatorDeclaration:
+                    return ((OperatorDeclarationSyntax)node).OperatorToken.GetLocation();
+                case SyntaxKind.DelegateDeclaration:
+                    return ((DelegateDeclarationSyntax)node).Identifier.GetLocation();
+                case SyntaxKind.EventDeclaration:
+                    return ((EventDeclarationSyntax)node).Identifier.GetLocation();
+                case SyntaxKind.IndexerDeclaration:
+                    return ((IndexerDeclarationSyntax)node).ThisKeyword.GetLocation();
+                case SyntaxKind.PropertyDeclaration:
+                    return ((PropertyDeclarationSyntax)node).Identifier.GetLocation();
+                case SyntaxKind.EventFieldDeclaration:
+                    return ((EventFieldDeclarationSyntax)node).Declaration.Variables.First().Identifier
+                        .GetLocation();
+                case SyntaxKind.FieldDeclaration:
+                    return ((FieldDeclarationSyntax)node).Declaration.Variables.First().Identifier.GetLocation();
+                default:
+                    return null;
+            }
+        }
+
+        private static bool IsExplicitInterfaceImplementation(SyntaxNode node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.MethodDeclaration:
+                    return ((MethodDeclarationSyntax)node).ExplicitInterfaceSpecifier != null;
+                case SyntaxKind.EventDeclaration:
+                    return ((EventDeclarationSyntax)node).ExplicitInterfaceSpecifier != null;
+                case SyntaxKind.IndexerDeclaration:
+                    return ((IndexerDeclarationSyntax)node).ExplicitInterfaceSpecifier != null;
+                case SyntaxKind.PropertyDeclaration:
+                    return ((PropertyDeclarationSyntax)node).ExplicitInterfaceSpecifier != null;
+                default:
+                    return false;
             }
         }
     }
