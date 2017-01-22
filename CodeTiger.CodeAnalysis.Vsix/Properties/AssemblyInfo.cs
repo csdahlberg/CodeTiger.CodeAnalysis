bfbd149d9d2197b563e53565b0da5abb05fbd080
Chris Dahlberg <csdahlberg@gmail.com>
2017-01-22 08:25:49 -0600

    Added an AssemblyInfo.cs file to the CodeTiger.CodeAnalysis.Vsix project to avoid a build warning on VSTS.

---
+++ CodeTiger.CodeAnalysis.Vsix/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CodeTiger.CodeAnalysis.Vsix")]
+[assembly: AssemblyDescription("Contains Roslyn-based static code analysis for C# projects.")]
+[assembly: AssemblyCompany("CodeTiger")]
+[assembly: AssemblyProduct("CodeTiger Code Analysis")]
+[assembly: AssemblyCopyright("Copyright © 2015 Chris Dahlberg")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en-US")]
+
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyInformationalVersion("0.0.1-unofficial")]
