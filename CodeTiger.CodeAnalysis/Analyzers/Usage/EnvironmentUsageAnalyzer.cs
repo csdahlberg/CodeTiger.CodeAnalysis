8d8de75447936d8b9d209bba7b17deff18ccfe3f
Chris Dahlberg <csdahlberg@gmail.com>
2017-07-23 20:23:10 -0500

    Added CT2203 to identify usage of Environment.Exit.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Usage/EnvironmentUsageAnalyzer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Usage
+{
+    /// <summary>
+    /// Analyzes usage of the <see cref="Environment"/> class.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class EnvironmentUsageAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor EnvironmentExitShouldNotBeUsedDescriptor
+            = new DiagnosticDescriptor("CT2203", "Environment.Exit should not be used.",
+                "Environment.Exit should not be used.", "CodeTiger.Usage", DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Returns a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(EnvironmentExitShouldNotBeUsedDescriptor);
+            }
+        }
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSemanticModelAction(AnalyzeEnvironmentUsage);
+        }
+
+        private void AnalyzeEnvironmentUsage(SemanticModelAnalysisContext context)
+        {
+            var root = context.SemanticModel.SyntaxTree.GetRoot(context.CancellationToken);
+            var environmentType = context.SemanticModel.Compilation.GetTypeByMetadataName("System.Environment");
+            if (environmentType == null)
+            {
+                return;
+            }
+
+            var invocations = root.DescendantNodes().OfType<InvocationExpressionSyntax>();
+
+            foreach (var invocation in invocations
+                .Where(x => x.Expression.Kind() == SyntaxKind.SimpleMemberAccessExpression))
+            {
+                var memberAccessExpression = (MemberAccessExpressionSyntax)invocation.Expression;
+
+                var targetType = context.SemanticModel.GetTypeInfo(memberAccessExpression.Expression);
+                if (targetType.Type == environmentType && memberAccessExpression?.Name?.Identifier.Text == "Exit")
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(EnvironmentExitShouldNotBeUsedDescriptor,
+                        memberAccessExpression.Expression.GetLocation()));
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTests.CodeTiger.CodeAnalysis/Analyzers/Usage/EnvironmentUsageAnalyzerTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeTiger.CodeAnalysis.Analyzers.Usage;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace UnitTests.CodeTiger.CodeAnalysis.Analyzers.Usage
+{
+    public class EnvironmentUsageAnalyzerTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void CallingMethodOtherThanExitDoesNotProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            Environment.ExpandEnvironmentVariables(""%userprofile%"");
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void CallingExitProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            Environment.Exit(0);
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT2203",
+                    Message = "Environment.Exit should not be used.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 8, 13)
+                    }
+                });
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new EnvironmentUsageAnalyzer();
+        }
+    }
+}
