553bbfa0c156b7cf517a8373949d505e245ef041
Chris Dahlberg <csdahlberg@gmail.com>
2017-08-01 07:08:16 -0500

    Added CT3113 to identify literals on the left side of comparison operators.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Readability/ComparisonReadabilityAnalyzer.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Readability
+{
+    /// <summary>
+    /// Analyzes comparisons for readability issues.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ComparisonReadabilityAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor
+            LiteralsShouldNotBeOnTheLeftSideOfComparisonOperatorsDescriptor = new DiagnosticDescriptor("CT3113",
+                "Literals should not be on the left side of comparison operators.",
+                "Literals should not be on the left side of comparison operators.", "CodeTiger.Readability",
+                DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(
+                    LiteralsShouldNotBeOnTheLeftSideOfComparisonOperatorsDescriptor);
+            }
+        }
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeComparison, SyntaxKind.EqualsExpression,
+                SyntaxKind.NotEqualsExpression, SyntaxKind.GreaterThanExpression,
+                SyntaxKind.GreaterThanOrEqualExpression, SyntaxKind.LessThanExpression,
+                SyntaxKind.LessThanOrEqualExpression);
+        }
+
+        private void AnalyzeComparison(SyntaxNodeAnalysisContext context)
+        {
+            var node = (BinaryExpressionSyntax)context.Node;
+
+            if (IsLiteral(node.Left) && !IsLiteral(node.Right))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(
+                    LiteralsShouldNotBeOnTheLeftSideOfComparisonOperatorsDescriptor, node.GetLocation()));
+            }
+        }
+
+        private bool IsLiteral(ExpressionSyntax node)
+        {
+            switch (node.Kind())
+            {
+                case SyntaxKind.CharacterLiteralExpression:
+                case SyntaxKind.FalseLiteralExpression:
+                case SyntaxKind.NullLiteralExpression:
+                case SyntaxKind.NumericLiteralExpression:
+                case SyntaxKind.StringLiteralExpression:
+                case SyntaxKind.TrueLiteralExpression:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTests.CodeTiger.CodeAnalysis/Analyzers/Readability/ComparisonReadabilityAnalyzerTests.cs
@@ -0,0 +1,125 @@
+﻿using CodeTiger.CodeAnalysis.Analyzers.Readability;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using Xunit;
+
+namespace UnitTests.CodeTiger.CodeAnalysis.Analyzers.Readability
+{
+    public class ComparisonReadabilityAnalyzerTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void CharacterComparisonsWithLiteralOnRightDoesNotProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void DoSomething(char charValue, bool boolValue, string stringValue, int intValue)
+        {
+            if (charValue <= 'C'
+                || boolValue == true
+                || boolValue != false
+                || stringValue == null
+                || stringValue != """"
+                || intValue > 1)
+            {
+            }
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(Tuple.Create("TestFile.cs", code));
+        }
+
+        [Fact]
+        public void CharacterComparisonsWithLiteralOnLeftProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void DoSomething(char charValue, bool boolValue, string stringValue, int intValue)
+        {
+            if ('C' >= charValue
+                || true == boolValue
+                || false != boolValue
+                || null == stringValue
+                || """" != stringValue
+                || 1 < intValue)
+            {
+            }
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT3113",
+                    Message = "Literals should not be on the left side of comparison operators.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 8, 17)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3113",
+                    Message = "Literals should not be on the left side of comparison operators.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 20)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3113",
+                    Message = "Literals should not be on the left side of comparison operators.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 10, 20)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3113",
+                    Message = "Literals should not be on the left side of comparison operators.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 11, 20)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3113",
+                    Message = "Literals should not be on the left side of comparison operators.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 12, 20)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3113",
+                    Message = "Literals should not be on the left side of comparison operators.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 13, 20)
+                    }
+                });
+        }
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ComparisonReadabilityAnalyzer();
+        }
+    }
+}
