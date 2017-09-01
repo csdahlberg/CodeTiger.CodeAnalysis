bff2b1e0e8f0b40b04bc89d9d2f389af3edb5bae
Chris Dahlberg <csdahlberg@gmail.com>
2017-08-31 20:34:58 -0500

    Added CT3526 to identify multiple attributes declared in the same attribute list.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Layout/AttributeLayoutAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Layout
+{
+    /// <summary>
+    /// Analyzes attributes for layout issues.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AttributeLayoutAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor AttributesShouldBeDeclaredSeparatelyDescriptor
+            = new DiagnosticDescriptor("CT3526", "Attributes should be declared separately.",
+                "Attributes should be declared separately.", "CodeTiger.Layout", DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create<DiagnosticDescriptor>(AttributesShouldBeDeclaredSeparatelyDescriptor);
+            }
+        }
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeAttributeList, SyntaxKind.AttributeList);
+        }
+
+        private void AnalyzeAttributeList(SyntaxNodeAnalysisContext context)
+        {
+            var node = (AttributeListSyntax)context.Node;
+
+            if (node.Attributes.Count > 1)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(AttributesShouldBeDeclaredSeparatelyDescriptor,
+                    node.Attributes[0].GetLocation(), node.Attributes.Skip(1).Select(x => x.GetLocation())));
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTests.CodeTiger.CodeAnalysis/Analyzers/Layout/AttributeLayoutAnalyzerTests.cs
@@ -0,0 +1,78 @@
+﻿using CodeTiger.CodeAnalysis.Analyzers.Layout;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace UnitTests.CodeTiger.CodeAnalysis.Analyzers.Layout
+{
+    public class AttributeLayoutAnalyzerTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void AttributesDeclaredSeparatelyDoNotProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    [Obsolete]
+    [CLSCompliant(false)]
+    public class Class1
+    {
+        [Obsolete]
+        [CLSCompliant(false)]
+        public void DoSomething()
+        {
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void AttributesDeclaredInSameAttributeListProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    [Obsolete, CLSCompliant(false)]
+    public class Class1
+    {
+        [Obsolete, CLSCompliant(false)]
+        public void DoSomething()
+        {
+        }
+    }
+}
+";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT3526",
+                    Message = "Attributes should be declared separately.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 4, 6),
+                        new DiagnosticResultLocation("Test0.cs", 4, 16)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3526",
+                    Message = "Attributes should be declared separately.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 7, 10),
+                        new DiagnosticResultLocation("Test0.cs", 7, 20)
+                    }
+                });
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AttributeLayoutAnalyzer();
+        }
+    }
+}
