2e1028579fcddf05b8dfae657fc18b14383974f7
Chris Dahlberg <csdahlberg@gmail.com>
2017-09-03 09:32:40 -0500

    Added CT3531 to identify lines of code that exceed the maximum length.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Layout/LineLengthLayoutAnalyzer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Layout
+{
+    /// <summary>
+    /// Analyzes the length of lines of code.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class LineLengthLayoutAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor LinesShouldNotExceedTheMaximumLengthDescriptor
+            = new DiagnosticDescriptor("CT3531", "Lines should not exceed the maximum length of 115.",
+                "Lines should not exceed the maximum length of 115.", "CodeTiger.Layout",
+                DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(LinesShouldNotExceedTheMaximumLengthDescriptor);
+            }
+        }
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxTreeAction(AnalyzeSyntaxTree);
+        }
+
+        private static void AnalyzeSyntaxTree(SyntaxTreeAnalysisContext context)
+        {
+            var text = context.Tree.GetText(context.CancellationToken);
+
+            foreach (var line in text.Lines)
+            {
+                int lineColumns = 0;
+                int firstCharacterExceedingMaxLength = -1;
+                for (int i = line.Span.Start; i < line.Span.End; i += 1)
+                {
+                    if (line.Text[i] == '\t')
+                    {
+                        lineColumns += 4;
+                    }
+                    else
+                    {
+                        lineColumns += 1;
+                    }
+
+                    if (lineColumns > 115)
+                    {
+                        firstCharacterExceedingMaxLength = i;
+                        break;
+                    }
+                }
+
+                if (firstCharacterExceedingMaxLength > 0)
+                {
+                    var lineSpanExceedingMaxLength = TextSpan.FromBounds(firstCharacterExceedingMaxLength,
+                        line.End);
+                    context.ReportDiagnostic(Diagnostic.Create(LinesShouldNotExceedTheMaximumLengthDescriptor,
+                        Location.Create(context.Tree, lineSpanExceedingMaxLength)));
+                }
+            }
+        }
+    }
+}
---
+++ UnitTests/UnitTests.CodeTiger.CodeAnalysis/Analyzers/Layout/LineLengthLayoutAnalyzerTests.cs
@@ -0,0 +1,91 @@
+﻿using CodeTiger.CodeAnalysis.Analyzers.Layout;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace UnitTests.CodeTiger.CodeAnalysis.Analyzers.Layout
+{
+    public class LineLengthLayoutAnalyzerTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void LinesShorterOrEqualToTheMaximumLengthDoNotProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1.Namespace0000000000000000000000000000000000000000000000000000000000000000000000000000000000
+{
+	public class Class111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(""                                                                                 "");
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void LinesLongerThanTheMaximumLengthProduceDiagnostics()
+        {
+            string code = @"using System;
+namespace ClassLibrary1.Namespace00000000000000000000000000000000000000000000000000000000000000000000000000000000000
+{
+	public class Class1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111
+    {
+        public void DoSomething22222222222222222222222222222222222222222222222222222222222222222222222222222222222()
+        {
+            Console.WriteLine(""                                                                                  "");
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT3531",
+                    Message = "Lines should not exceed the maximum length of 115.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 2, 116)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3531",
+                    Message = "Lines should not exceed the maximum length of 115.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 4, 113)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3531",
+                    Message = "Lines should not exceed the maximum length of 115.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 6, 116)
+                    }
+                },
+                new DiagnosticResult
+                {
+                    Id = "CT3531",
+                    Message = "Lines should not exceed the maximum length of 115.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 8, 116)
+                    }
+                });
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new LineLengthLayoutAnalyzer();
+        }
+    }
+}
