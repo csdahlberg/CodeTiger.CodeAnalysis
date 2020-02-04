ea962ffbdb162c631dae379c04cc99f74d068e11
Chris Dahlberg <csdahlberg@gmail.com>
2020-02-03 23:13:01 -0600

    Created the CodeTiger.CodeAnalysis.SelfTest solution.

---
+++ SelfTests/UnitTests.CodeTiger.CodeAnalysis.SelfTest/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeTiger.Naming", "CT1702:Type names should use pascal casing.",
+    Justification = "Unit test classes are allowed to contain underscores.")]
