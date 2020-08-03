2af3836d5020331faadaa19fc98150e8f0cef1db
Chris Dahlberg <csdahlberg@gmail.com>
2020-08-02 20:17:34 -0500

    Added CT1020 to identify extension methods that do not use the 'this' parameter.

--- CodeTiger.CodeAnalysis/Analyzers/Design/ParameterDesignAnalyzer.cs
+++ CodeTiger.CodeAnalysis/Analyzers/Design/ParameterDesignAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Immutable;
+using System.Linq;
 using CodeTiger.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -17,12 +18,18 @@ namespace CodeTiger.CodeAnalysis.Analyzers.Design
             = new DiagnosticDescriptor("CT1003", "Methods should not exceed seven parameters.",
                 "Methods should not exceed seven parameters.", "CodeTiger.Design", DiagnosticSeverity.Warning,
                 true);
+        internal static readonly DiagnosticDescriptor ExtensionMethodsShouldUseTheThisParameterDescriptor
+            = new DiagnosticDescriptor("CT1020", "Extension methods should use the 'this' parameter.",
+                "Extension methods should use the 'this' parameter.", "CodeTiger.Design",
+                DiagnosticSeverity.Warning, true);
 
         /// <summary>
         /// Gets a set of descriptors for the diagnostics that this analyzer is capable of producing.
         /// </summary>
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
-            => ImmutableArray.Create(MethodsShouldNotExceedSevenParametersDescriptor);
+            => ImmutableArray.Create(
+                MethodsShouldNotExceedSevenParametersDescriptor,
+                ExtensionMethodsShouldUseTheThisParameterDescriptor);
 
         /// <summary>
         /// Registers actions in an analysis context.
@@ -40,6 +47,7 @@ namespace CodeTiger.CodeAnalysis.Analyzers.Design
                 SyntaxKind.ConversionOperatorDeclaration, SyntaxKind.DelegateDeclaration,
                 SyntaxKind.IndexerDeclaration, SyntaxKind.MethodDeclaration,
                 SyntaxKind.OperatorDeclaration);
+            context.RegisterSyntaxNodeAction(AnalyzeMethodForThisParameter, SyntaxKind.MethodDeclaration);
         }
 
         private static void AnalyzeParameterCount(SyntaxNodeAnalysisContext context)
@@ -48,6 +56,37 @@ namespace CodeTiger.CodeAnalysis.Analyzers.Design
             {
                 context.ReportDiagnostic(Diagnostic.Create(MethodsShouldNotExceedSevenParametersDescriptor,
                     context.Node.GetIdentifierLocation()));
+            }
+        }
+
+        private static void AnalyzeMethodForThisParameter(SyntaxNodeAnalysisContext context)
+        {
+            var node = (MethodDeclarationSyntax)context.Node;
+
+            if (node.Body == null && node.ExpressionBody == null)
+            {
+                return;
+            }
+
+            var methodDeclaration = context.SemanticModel.GetDeclaredSymbol(node, context.CancellationToken);
+
+            if (methodDeclaration.IsExtensionMethod)
+            {
+                var thisParameterNode = node.ParameterList.Parameters
+                    .Single(x => x.Modifiers.Any(SyntaxKind.ThisKeyword));
+                var thisParameter = context.SemanticModel
+                    .GetDeclaredSymbol(thisParameterNode, context.CancellationToken);
+
+                DataFlowAnalysis dataFlow = node.Body != null
+                    ? context.SemanticModel.AnalyzeDataFlow(node.Body)
+                    : context.SemanticModel.AnalyzeDataFlow(node.ExpressionBody);
+
+                if (!dataFlow.ReadInside.Contains(thisParameter)
+                    && !dataFlow.WrittenInside.Contains(thisParameter))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(ExtensionMethodsShouldUseTheThisParameterDescriptor,
+                        node.Identifier.GetLocation()));
+                }
             }
         }
 
---
+++ UnitTests/UnitTests.CodeTiger.CodeAnalysis/Analyzers/Design/ParameterDesignAnalyzerTests.cs
@@ -0,0 +1,52 @@
+﻿using CodeTiger.CodeAnalysis.Analyzers.Design;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace UnitTests.CodeTiger.CodeAnalysis.Analyzers.Design
+{
+    public class ParameterDesignAnalyzerTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void ExtensionMethodThatUsesTheThisParameterDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public static class StringExtensions
+    {
+        public static string WithAsterisk(this string original) { return original + ""*""; }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void ExtensionMethodThatDoesNotUseTheThisParameterProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public static class StringExtensions
+    {
+        public static string WithAsterisk(this string original) { return ""*""; }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT1020",
+                    Message = "Extension methods should use the 'this' parameter.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[] { new DiagnosticResultLocation("Test0.cs", 6, 30) }
+                });
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ParameterDesignAnalyzer();
+        }
+    }
+}
