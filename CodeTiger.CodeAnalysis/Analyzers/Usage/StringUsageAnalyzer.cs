c253caf1097460befca95a28ef82d8ae876fdef7
Chris Dahlberg <csdahlberg@gmail.com>
2021-02-13 11:58:23 -0600

    Added CT2208 to identify string literals that should be interpolated strings.

---
+++ CodeTiger.CodeAnalysis/Analyzers/Usage/StringUsageAnalyzer.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeTiger.CodeAnalysis.Analyzers.Usage
+{
+    /// <summary>
+    /// Analyzes usages of <see cref="string"/> and related types.
+    /// </summary>
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class StringUsageAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly DiagnosticDescriptor InterpolatedStringsRequireLeadingDollarSignDescriptor
+            = new DiagnosticDescriptor("CT2208", "Interpolated strings require a leading '$' character.",
+                "Interpolated strings require a leading '$' character.", "CodeTiger.Usage",
+                DiagnosticSeverity.Warning, true);
+
+        /// <summary>
+        /// Returns a set of descriptors for the diagnostics that this analyzer is capable of producing.
+        /// </summary>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+            => ImmutableArray.Create(InterpolatedStringsRequireLeadingDollarSignDescriptor);
+
+        /// <summary>
+        /// Registers actions in an analysis context.
+        /// </summary>
+        /// <param name="context">The context to register actions in.</param>
+        /// <remarks>This method should only be called once, at the start of a session.</remarks>
+        public override void Initialize(AnalysisContext context)
+        {
+            Guard.ArgumentIsNotNull(nameof(context), context);
+
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeStringLiteral, SyntaxKind.StringLiteralExpression);
+        }
+
+        private static void AnalyzeStringLiteral(SyntaxNodeAnalysisContext context)
+        {
+            var node = (LiteralExpressionSyntax)context.Node;
+
+            string value = (string)node.Token.Text;
+
+            if (value.Contains("{") && value.Contains("}"))
+            {
+                var newNode = SyntaxFactory.ExpressionStatement(
+                    SyntaxFactory.AssignmentExpression(SyntaxKind.SimpleAssignmentExpression,
+                        SyntaxFactory.IdentifierName("_"),
+                        SyntaxFactory.Token(SyntaxKind.EqualsToken),
+                        SyntaxFactory.ParseExpression("$" + value)));
+                if (context.SemanticModel
+                    .TryGetSpeculativeSemanticModel(node.SpanStart, newNode, out var speculativeModel))
+                {
+                    AnalyzeInterpolatedStringFromStringLiteral(context, node, speculativeModel);
+                }
+            }
+        }
+
+        private static void AnalyzeInterpolatedStringFromStringLiteral(SyntaxNodeAnalysisContext context,
+            LiteralExpressionSyntax node, SemanticModel speculativeModel)
+        {
+            var speculativeRoot = speculativeModel.SyntaxTree.GetRoot(context.CancellationToken);
+            var expressionStatementNode = speculativeRoot as ExpressionStatementSyntax;
+            var assignmentExpressionNode = expressionStatementNode?.Expression as AssignmentExpressionSyntax;
+            var interpolatedStringExpressionNode = assignmentExpressionNode?.Right
+                as InterpolatedStringExpressionSyntax;
+
+            if (interpolatedStringExpressionNode == null)
+            {
+                return;
+            }
+
+            for (int i = 0; i < interpolatedStringExpressionNode.Contents.Count; i += 1)
+            {
+                if (interpolatedStringExpressionNode.Contents[i] is InterpolationSyntax interpolationNode
+                    && IsInterpolationProbablyValid(context, speculativeModel, interpolationNode))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(
+                        InterpolatedStringsRequireLeadingDollarSignDescriptor,
+                        node.GetLocation()));
+                    return;
+                }
+            }
+        }
+
+        private static bool IsInterpolationProbablyValid(SyntaxNodeAnalysisContext context,
+            SemanticModel speculativeModel, InterpolationSyntax interpolationNode)
+        {
+            if (interpolationNode.Expression is IdentifierNameSyntax identifierNode)
+            {
+                var identifier = speculativeModel
+                    .GetSymbolInfo(identifierNode, context.CancellationToken).Symbol;
+
+                return identifier != null;
+            }
+            else if (interpolationNode.Expression is MemberAccessExpressionSyntax memberAccessExpressionNode)
+            {
+                var identifier = speculativeModel
+                    .GetSymbolInfo(memberAccessExpressionNode, context.CancellationToken).Symbol;
+
+                return identifier != null;
+            }
+            else if (interpolationNode.Expression is InvocationExpressionSyntax invocationExpressionNode)
+            {
+                var identifier = speculativeModel
+                    .GetSymbolInfo(invocationExpressionNode.Expression, context.CancellationToken).Symbol;
+                if (identifier != null)
+                {
+                    return true;
+                }
+
+                // Some compile-time expressions like nameof expressions do not have a resolvable symbol, but do
+                // have a constant value.
+                return speculativeModel.GetConstantValue(invocationExpressionNode, context.CancellationToken)
+                    .HasValue;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTests/UnitTests.CodeTiger.CodeAnalysis/Analyzers/Usage/StringUsageAnalyzerTests.cs
@@ -0,0 +1,269 @@
+﻿using CodeTiger.CodeAnalysis.Analyzers.Usage;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Xunit;
+
+namespace UnitTests.CodeTiger.CodeAnalysis.Analyzers.Usage
+{
+    public class StringUsageAnalyzerTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void EmptyStringLiteralDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            string value = """";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void StringLiteralWithUnbracedLocalVariableDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            int x = 13;
+            string value = ""x"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void StringLiteralWithBracedLocalVariableProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            int x = 13;
+            string value = ""{x}"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT2208",
+                    Message = "Interpolated strings require a leading '$' character.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 28)
+                    }
+                });
+        }
+
+        [Fact]
+        public void StringLiteralWithUnbracedFieldDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        int x = 13;
+        public void TestMethod()
+        {
+            string value = ""x"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void StringLiteralWithBracedFieldProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            int x = 13;
+            string value = ""{x}"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT2208",
+                    Message = "Interpolated strings require a leading '$' character.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 28)
+                    }
+                });
+        }
+
+        [Fact]
+        public void StringLiteralWithUnbracedParameterDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod(int x)
+        {
+            string value = ""x"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void StringLiteralWithBracedParameterProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod(int x)
+        {
+            string value = ""{x}"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT2208",
+                    Message = "Interpolated strings require a leading '$' character.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 8, 28)
+                    }
+                });
+        }
+
+        [Fact]
+        public void StringLiteralWithUnbracedNameofExpressionDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            string value = ""nameof(TestMethod)"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void StringLiteralWithBracedNameofExpressionProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public void TestMethod()
+        {
+            string value = ""{nameof(TestMethod)}"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT2208",
+                    Message = "Interpolated strings require a leading '$' character.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 8, 28)
+                    }
+                });
+        }
+
+        [Fact]
+        public void StringLiteralWithUnbracedMemberAccessExpressionDoesNotProduceDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public static int X;
+        public void TestMethod()
+        {
+            string value = ""TestClass.X"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code);
+        }
+
+        [Fact]
+        public void StringLiteralWithBracedMemberAccessExpressionProducesDiagnostic()
+        {
+            string code = @"using System;
+namespace ClassLibrary1
+{
+    public class TestClass
+    {
+        public static int X;
+        public void TestMethod()
+        {
+            string value = ""{TestClass.X}"";
+        }
+    }
+}";
+
+            VerifyCSharpDiagnostic(code,
+                new DiagnosticResult
+                {
+                    Id = "CT2208",
+                    Message = "Interpolated strings require a leading '$' character.",
+                    Severity = DiagnosticSeverity.Warning,
+                    Locations = new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 9, 28)
+                    }
+                });
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new StringUsageAnalyzer();
+        }
+    }
+}
