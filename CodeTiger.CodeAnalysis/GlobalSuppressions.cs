f0c27a856a1b0e793b4be7b732b7e9e047da4362
Chris Dahlberg <csdahlberg@gmail.com>
2025-04-27 08:37:04 -0500

    Suppressed a message about the namespace for System.* types (used to enable some language features) not matching the folder structure.

---
+++ CodeTiger.CodeAnalysis/GlobalSuppressions.cs
@@ -0,0 +1,5 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0130:Namespace does not match folder structure", Scope = "namespace",
+    Target = "~N:System",
+    Justification = "Some types need to be created in the System namespace to enable newer language features.")]
